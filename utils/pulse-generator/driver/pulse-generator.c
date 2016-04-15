b9084744333fa84742446f7f58c7fe7f4199fdf3
rascol <rayconnell@usa.net>
2016-04-15 17:46:49 -0400

    Restructured tree and added pulse-generator files.

---
+++ utils/pulse-generator/driver/pulse-generator.c
@@ -0,0 +1,291 @@
+/*
+ * pulse-generator.c
+ *
+ * Created on: Apr 9, 2016
+ * Copyright (C) 2016  Raymond S. Connell
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * pulse-generator.ko must be copied to
+ *  /lib/modules/`uname -r`/kernel/drivers/misc/pulse-generator.ko
+ */
+
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+
+#include <linux/sched.h>
+#include <linux/kernel.h>	/* printk() */
+#include <linux/fs.h>		/* everything... */
+#include <linux/errno.h>	/* error codes */
+#include <linux/delay.h>	/* udelay */
+#include <linux/kdev_t.h>
+#include <linux/slab.h>
+#include <linux/mm.h>
+#include <linux/ioport.h>
+#include <linux/interrupt.h>
+#include <linux/workqueue.h>
+#include <linux/poll.h>
+#include <linux/wait.h>
+#include <linux/param.h>
+#include <asm/gpio.h>
+#include <asm/atomic.h>
+#include <asm/io.h>
+#include <../kernel/time/timekeeping.h>
+
+/* The text below will appear in output from 'cat /proc/interrupt' */
+#define MODULE_NAME "pulse-generator"
+
+const char *version = "pulse-generator-driver v0.1.0";
+
+static int major = 0;								/* dynamic by default */
+module_param(major, int, 0);						/* but can be specified at load time */
+
+static int gpio_num1 = 0;
+module_param(gpio_num1, int, 0);						/* Specify gpio 1 at load time */
+
+static int gpio_num2 = 0;
+module_param(gpio_num2, int, 0);						/* Specify gpio 2 at load time */
+
+MODULE_AUTHOR ("Raymond Connell");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_VERSION("0.1.0");
+
+int gpio_out1 = 0;
+int gpio_out2 = 0;
+
+int pulseTime[2];
+
+static atomic_t driver_available = ATOMIC_INIT(1);
+
+/**
+ * Permits open() only by the first caller until
+ * that caller closes the driver.
+ */
+int pulsegen_open (struct inode *inode, struct file *filp)
+{
+	/**
+	 * The following statement fails if driver_available
+	 * is 1 and driver_available then gets set to zero.
+	 *
+	 * The statement succeeds if driver_available is 0
+	 * and driver_available then gets set to -1.
+	 */
+    if (! atomic_dec_and_test(&driver_available)) {
+
+    	/**
+    	 * If driver_available was initially 0 then got
+    	 * here and driver_available was set to -1. But
+    	 * the next statment sets driver_available back
+    	 * to 0. So every subsequent open call comes here.
+    	 */
+
+        atomic_inc(&driver_available);
+        return -EBUSY; 						/* already open */
+    }
+
+    /**
+     * If driver_available was initially 1 then got
+     * here and driver_available was set to 0
+     * by atomic_dec_and_test().
+     */
+    return 0;
+}
+
+/**
+ * Closes the driver but keeps it active so the next
+ * caller can open it again.
+ */
+int pulsegen_release(struct inode *inode, struct file *filp)
+{
+	/**
+	 * Sets driver_available to 1 so the next caller
+	 * can open the driver again after this close.
+	 */
+	atomic_inc(&driver_available);
+	return 0;
+}
+
+/**
+ * Asserts a 50 usec pulse at the fractional second time
+ * requested by timeout on the gpio given by gpio_out.
+ */
+void generate_pulse(int timeout, int *gpio_out)
+{
+	struct timeval tv;
+	int startTime, lastTime, time;
+
+	do_gettimeofday(&tv);
+	startTime = tv.tv_usec;
+	if (startTime > timeout){				// At the rollover of the second,
+		timeout += 1000000;					// extend the timeout value
+	}
+
+	lastTime = startTime;
+
+	do {									// Spin until time of day >= timeout
+		do_gettimeofday(&tv);
+
+		time = tv.tv_usec;
+		if (time < lastTime){				// If the second rolled over,
+			time += 1000000;				// extend the time value.
+		}
+
+		lastTime = time;
+
+	} while (time < timeout);
+
+	pulseTime[0] = tv.tv_sec;				// Record time of rising edge
+	pulseTime[1] = tv.tv_usec;
+
+	gpio_set_value(*gpio_out, 1);
+
+	timeout += 50;
+
+	do {									// Spin until time of day >= timeout
+		do_gettimeofday(&tv);
+
+		time = tv.tv_usec;
+		if (time < lastTime){				// If the second rolled over,
+			time += 1000000;				// extend the time value.
+		}
+
+		lastTime = time;
+
+	} while (time < timeout);
+
+	gpio_set_value(*gpio_out, 0);
+}
+
+/**
+ * Asserts up to two 50 usec pulses at the fractional second
+ * times requested by buf as a pair of integers. The count
+ * value must be 2 * sizeof(int).
+ */
+ssize_t pulsegen_i_write (struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
+{
+   /**
+	*  For future reference:
+	*
+	*  void gpio_set_value(unsigned gpio, int value)
+	*
+	*  int gpio_get_value(unsigned gpio)
+	*
+	*  are the functions to set output and get input
+	*  values from appropriate gpio pins.
+	*/
+
+	int *timeData = (int *)buf;				// timeData[0] : first or second GPIO, timeData[1] : pulse time
+
+	if (timeData[0] == 0){
+		generate_pulse(timeData[1], &gpio_out1);
+	}
+	else{
+		generate_pulse(timeData[1], &gpio_out2);
+	}
+
+	return 0;
+}
+
+ssize_t pulsegen_i_read (struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+{
+	if (copy_to_user(buf, (char *)pulseTime, count)){
+		return -EFAULT;
+	}
+
+	return count;
+}
+
+struct file_operations pulsegen_i_fops = {
+	.owner	 = THIS_MODULE,
+	.write   = pulsegen_i_write,
+	.read	 = pulsegen_i_read,
+	.open	 = pulsegen_open,
+	.release = pulsegen_release,
+};
+
+int configureWriteOn(int gpio_num){
+
+	if (gpio_request(gpio_num, MODULE_NAME)) {
+		printk(KERN_INFO "pulse-generator: GPIO request failed on GPIO %d\n", gpio_num);
+		return -1;
+	}
+
+	if (gpio_direction_output(gpio_num, 0)){
+		printk(KERN_INFO "pulse-generator: GPIO pin direction 'OUTPUT' on GPIO %d was denied\n", gpio_num);
+		return -1;
+	}
+
+	return 0;
+}
+
+void pulsegen_cleanup(void)
+{
+	flush_scheduled_work();
+
+	unregister_chrdev(major, "pulse-generator");
+
+	gpio_free(gpio_num1);
+
+	if (gpio_num2 > 0){
+		gpio_free(gpio_num2);
+	}
+
+	printk(KERN_INFO "pulse-generator: removed\n");
+}
+
+int pulsegen_init(void)
+{
+	int result;
+
+	result = register_chrdev(major, "pulse-generator", &pulsegen_i_fops);
+	if (result < 0) {
+		printk(KERN_INFO "pulse-generator: can't get major number\n");
+		return result;
+	}
+
+	if (major == 0)
+		major = result; /* dynamic */
+
+	if (configureWriteOn(gpio_num1) == -1){
+		printk(KERN_INFO "pulse-generator: failed installation\n");
+		pulsegen_cleanup();
+		return -1;
+	}
+	gpio_out1 = gpio_num1;
+
+	if (gpio_num2 > 0){
+		if (configureWriteOn(gpio_num2) == -1){
+			printk(KERN_INFO "pulse-generator: failed installation\n");
+			pulsegen_cleanup();
+			return -1;
+		}
+		gpio_out2 = gpio_num2;
+	}
+
+	printk(KERN_INFO "pulse-generator: installed\n");
+
+	pulseTime[0] = 0;
+	pulseTime[1] = 0;
+
+	return 0;
+}
+
+module_init(pulsegen_init);
+module_exit(pulsegen_cleanup);
+
+
+
+
---
+++ utils/pulse-generator/pulse-generator.cpp
@@ -0,0 +1,381 @@
+/*
+ * pulse-generator.cpp
+ *
+ * Created on: Apr 9, 2016
+ * Copyright (C) 2016  Raymond S. Connell
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sched.h>
+#include <sys/time.h>
+#include <fcntl.h>
+
+#define USECS_PER_SEC 1000000
+#define ON_TIME 0
+#define DELAYED 1
+#define SENTINEL 2
+
+const char *version = "pulse-generator v0.1.0";
+
+struct pulseGeneratorGlobalVars {
+	char strbuf[200];
+	int pulseTime1;
+	int pulseTime2;
+} g;
+
+const char *pulse_verify_file = "./PulseVerify";
+
+
+/**
+ * Reads the major number assigned to pulse-generator
+ * from "/proc/devices" as a string which is
+ * returned in the majorPos char pointer. This
+ * value is used to load the hardware driver that
+ * pulse-generator requires.
+ */
+char *copyMajorTo(char *majorPos){
+
+	struct stat stat_buf;
+
+	const char *filename = "/run/shm/proc_devices";
+
+	system("cat /proc/devices > /run/shm/proc_devices"); 	// "/proc/devices" can't be handled like
+															// a normal file so we copy it to a file.
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1){
+		return NULL;
+	}
+
+	fstat(fd, &stat_buf);
+	int sz = stat_buf.st_size;
+
+	char *fbuf = new char[sz+1];
+
+	int rv = read(fd, fbuf, sz);
+	if (rv == -1){
+		close(fd);
+		remove(filename);
+		delete(fbuf);
+		return NULL;
+	}
+	close(fd);
+	remove(filename);
+
+	fbuf[sz] = '\0';
+
+	char *pos = strstr(fbuf, "pulse-generator");
+	if (pos == NULL){
+		printf("Can't find pulse-generator in \"/run/shm/proc_devices\"\n");
+		delete fbuf;
+		return NULL;
+	}
+	char *end = pos - 1;
+	*end = 0;
+
+	pos -= 2;
+	char *pos2 = pos;
+	while (pos2 == pos){
+		pos -= 1;
+		pos2 = strpbrk(pos,"0123456789");
+	}
+	strcpy(majorPos, pos2);
+
+	delete fbuf;
+	return majorPos;
+}
+
+/**
+ * Loads the hardware driver required by pulse-generator which
+ * is expected to be available in the file:
+ * "/lib/modules/'uname -r'/kernel/drivers/misc/pulse-generator.ko".
+ */
+int driver_load(char *gpio1, char *gpio2){
+
+	memset(g.strbuf, 0, 200 * sizeof(char));
+
+	char *insmod = g.strbuf;
+
+	strcpy(insmod, "/sbin/insmod /lib/modules/`uname -r`/kernel/drivers/misc/pulse-generator.ko gpio_num1=");
+	strcat(insmod, gpio1);
+
+	if (gpio2 != NULL){
+		strcat(insmod, " gpio_num2=");
+		strcat(insmod, gpio2);
+	}
+
+	system("rm -f /dev/pulse-generator");			// Clean up any old device files.
+
+	system(insmod);									// Issue the insmod command
+
+	char *mknod = g.strbuf;
+	strcpy(mknod, "mknod /dev/pulse-generator c ");
+	char *major = copyMajorTo(mknod + strlen(mknod));
+	if (major == NULL){								// No major found! insmod failed.
+		printf("driver_load() error: No major found!\n");
+		system("/sbin/rmmod pulse-generator");
+		return -1;
+	}
+	strcat(mknod, " 0");
+
+	system(mknod);									// Issue the mknod command
+
+	system("chgrp root /dev/pulse-generator");
+	system("chmod 664 /dev/pulse-generator");
+
+	return 0;
+}
+
+/**
+ * Unloads the pulse-generator kernel driver.
+ */
+void driver_unload(void){
+	system("/sbin/rmmod pulse-generator");
+	system("rm -f /dev/pulse-generator");
+}
+
+/**
+ * Sets a nanosleep() time delay equal to the time remaining
+ * in the second from the time recorded as fracSec plus an
+ * adjustment value of timeAt in microseconds.
+ */
+struct timespec setSyncDelay(int timeAt, int fracSec){
+
+	struct timespec ts2;
+
+	int timerVal = USECS_PER_SEC + timeAt - fracSec;
+
+	if (timerVal >= USECS_PER_SEC){
+		ts2.tv_sec = 1;
+		ts2.tv_nsec = (timerVal - USECS_PER_SEC) * 1000;
+	}
+	else if (timerVal < 0){
+		ts2.tv_sec = 0;
+		ts2.tv_nsec = (USECS_PER_SEC + timerVal) * 1000;
+	}
+	else {
+		ts2.tv_sec = 0;
+		ts2.tv_nsec = timerVal * 1000;
+	}
+
+	return ts2;
+}
+
+void writeVerifyVal(int i){
+	int fd = open(pulse_verify_file, O_CREAT | O_WRONLY | O_TRUNC | O_SYNC, S_IRUSR | S_IWUSR | S_IROTH);
+	write(fd, &i, sizeof(int));
+	close(fd);
+}
+
+void writePulseStatus(int readData[], int pulseTime){
+	int pulseEnd = readData[1];
+	if (pulseEnd > pulseTime){
+		writeVerifyVal(DELAYED);
+	}
+	else {
+		writeVerifyVal(ON_TIME);
+	}
+}
+
+/**
+ * Generates one or two once-per-second pulses at the
+ * microsecond offsets given on the command line.
+ */
+int main(int argc, char *argv[]){
+
+	int pulseStart1 = 0, pulseStart2 = 0;
+	int writeData[2];
+	int readData[2];
+
+	struct timeval tv1;
+	struct timespec ts2;
+	int pulseEnd1, pulseEnd2;
+
+	memset(&g, 0, sizeof(struct pulseGeneratorGlobalVars));
+	g.pulseTime1 = -1;
+	g.pulseTime2 = -1;
+
+	int seq_num = 0;
+
+	if (argc > 1){
+		if (strcmp(argv[1], "load-driver") == 0){
+			if (geteuid() != 0){
+				printf("Requires superuser privileges. Please sudo this command.\n");
+				return 1;
+			}
+
+			if (argv[2] == NULL){
+				printf("GPIO number is a required second arg.\n");
+				printf("Could not load driver.\n");
+				return 1;
+			}
+
+			if (argc == 4){
+				if (driver_load(argv[2], argv[3]) == -1){
+					printf("Could not load pulse-generator driver. Exiting.\n");
+					return 1;
+				}
+			}
+			else {
+				if (driver_load(argv[2], NULL) == -1){
+					printf("Could not load pulse-generator driver. Exiting.\n");
+					return 1;
+				}
+			}
+
+			printf("pulse-generator: driver loaded\n");
+			return 0;
+		}
+		if (strcmp(argv[1], "unload-driver") == 0){
+			if (geteuid() != 0){
+				printf("Requires superuser privileges. Please sudo this command.\n");
+				return 1;
+			}
+
+			printf("pulse-generator: driver unloaded\n");
+			driver_unload();
+			return 0;
+		}
+		if (strcmp(argv[1], "-p") == 0 && argc == 3){
+			sscanf(argv[2], "%d", &g.pulseTime1);
+			if (g.pulseTime1 >= 0){
+				goto start;
+			}
+		}
+		if (strcmp(argv[1], "-p") == 0 && argc == 4){
+			sscanf(argv[2], "%d", &g.pulseTime1);
+			sscanf(argv[3], "%d", &g.pulseTime2);
+
+			if (g.pulseTime1 >= 0 && g.pulseTime2 > 0){
+				goto start;
+			}
+		}
+
+		printf("Usage:\n");
+		printf("Load driver with one or two output GPIOs:\n");
+		printf("  sudo pulse-generator load-driver <gpio-num1> [gpio-num2]\n");
+		printf("After loading the driver, calling pulse-generator\n");
+		printf("with the following flag and value(s) causes it to\n");
+		printf("generate one or two once-per-second pulse(s) at\n");
+		printf("specified time(s) offset from the rollover of the\n");
+		printf("second:\n");
+		printf("  -p <microseconds> [microseconds]\n");
+		printf("When the driver is no longer needed:\n");
+		printf("  sudo pulse-generator unload-driver\n");
+
+		return 0;
+	}
+
+start:
+	if (geteuid() != 0){
+		printf("Requires superuser privileges. Please sudo this command.\n");
+		return 1;
+	}
+
+	printf(version);
+	printf("\n");
+
+	struct sched_param param;						// Process must be run as
+	param.sched_priority = 99;						// root to change priority.
+	sched_setscheduler(0, SCHED_FIFO, &param);		// Else, this has no effect.
+
+	int fd = open("/dev/pulse-generator", O_RDWR);	// Open the pulse-generator device driver.
+	if (fd == -1){
+		printf("pulse-generator: Driver is not loaded. Exiting.\n");
+		return 1;
+	}
+
+	pulseStart1 = g.pulseTime1 - 75;
+	if (pulseStart1 < 0){
+		pulseStart1 = 1000000 + pulseStart1;
+	}
+
+	if (g.pulseTime2 > g.pulseTime1){
+		pulseStart2 = -(1000000 - g.pulseTime2) - 75;
+	}
+														// Set up a one-second delay loop that stays in synch by
+			    										// continuously re-timing to pulseStart1 value
+	gettimeofday(&tv1, NULL);
+	ts2 = setSyncDelay(pulseStart1, tv1.tv_usec);
+
+	char timeStr[100];
+	const char *timefmt = "%F %H:%M:%S";
+
+	for (;;){
+		nanosleep(&ts2, NULL);							// Sleep until ready to generate a pulse
+
+		writeData[0] = 0;
+		writeData[1] = g.pulseTime1;
+
+		write(fd, writeData, 2 * sizeof(int));			// Write the pulse data for g.pulseTime1
+
+		if (read(fd, readData, 2 * sizeof(int)) < 0){
+			printf("pulse-gemerator: Bad read from driver\n");
+			continue;
+		}
+
+		pulseEnd1 = readData[1];
+
+		if (g.pulseTime2 > g.pulseTime1){
+			gettimeofday(&tv1, NULL);
+			ts2 = setSyncDelay(pulseStart2, tv1.tv_usec);
+			nanosleep(&ts2, NULL);						// Sleep until ready to generate a pulse
+
+			writeData[0] = 1;
+			writeData[1] = g.pulseTime2;
+
+			write(fd, writeData, 2 * sizeof(int));		// Write the pulse data for g.pulseTime2
+
+			if (read(fd, readData, 2 * sizeof(int)) < 0){
+				printf("pulse-gemerator: Bad read from driver\n");
+				continue;
+			}
+
+			pulseEnd2 = readData[1];
+			writePulseStatus(readData, g.pulseTime2);
+
+			strftime(timeStr, 100, timefmt, localtime((const time_t*)(&(readData[0]))));
+			printf("%s %d %d\n", timeStr, pulseEnd1, pulseEnd2);
+		}
+		else {
+			writePulseStatus(readData, g.pulseTime1);
+
+			strftime(timeStr, 100, timefmt, localtime((const time_t*)(&(readData[0]))));
+			printf("%s %d\n", timeStr, pulseEnd1);
+		}
+
+		ts2.tv_sec = 0;									// Allow time for interrupt-timer to read verify file.
+		ts2.tv_nsec = 200000;
+		nanosleep(&ts2, NULL);
+
+		writeVerifyVal(SENTINEL);
+
+		gettimeofday(&tv1, NULL);
+		ts2 = setSyncDelay(pulseStart1, tv1.tv_usec);
+
+		seq_num += 1;
+	}
+
+	close(fd);
+
+	return 0;
+}
