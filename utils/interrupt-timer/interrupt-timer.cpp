62dbb01f719f95637835a717ee13fc33802b51d3
rascol <rayconnell@usa.net>
2016-04-18 13:29:23 -0400

    Updates to interrupt-timer source.

--- utils/interrupt-timer/interrupt-timer.cpp
+++ utils/interrupt-timer/interrupt-timer.cpp
@@ -29,17 +29,22 @@
 #include <time.h>
 #include <sched.h>
 
-#define INTRPT_DISTRIB_LEN 51
+#define INTRPT_DISTRIB_LEN 61
 #define SECS_PER_DAY 86400
 #define SECS_PER_MIN 60
 #define ON_TIME 0
 #define DELAYED 1
 #define SENTINEL 2
 #define FILE_NOT_FOUND -1
+#define HIDDEN_DELAY 1
+#define FILEBUF_SZ 5000
+#define STRBUF_SZ 200
 
 struct interruptTimerGlobalVars {
-	char strbuf[200];
+	int outFormat;
 
+	char strbuf[STRBUF_SZ];
+	char filebuf[FILEBUF_SZ];
 	int seconds;
 	int minutes;
 	int days;
@@ -50,6 +55,8 @@ struct interruptTimerGlobalVars {
 	int interruptDistrib[INTRPT_DISTRIB_LEN];
 
 	int lastIntrptFileno;
+
+	double probability;
 } g;
 
 const char *interrupt_distrib_file = "/var/local/intrpt-distrib-forming";
@@ -57,6 +64,7 @@ const char *last_intrpt_distrib_file = "/var/local/intrpt-distrib";
 const char *pulse_verify_file = "/mnt/pi/PulseVerify";
 
 const char *version = "interrupt-timer v0.1.0";
+const char *timefmt = "%F %H:%M:%S";
 
 /**
  * Reads the major number assigned to interrupt-timer
@@ -123,7 +131,7 @@ char *copyMajorTo(char *majorPos){
  */
 int driver_load(char *gpio){
 
-	memset(g.strbuf, 0, 200 * sizeof(char));
+	memset(g.strbuf, 0, STRBUF_SZ * sizeof(char));
 
 	char *insmod = g.strbuf;
 	strcpy(insmod, "/sbin/insmod /lib/modules/`uname -r`/kernel/drivers/misc/interrupt-timer.ko gpio_num=");
@@ -197,7 +205,6 @@ void writeInterruptDistribFile(void){
 	int scaleZero = -(g.scaleCenter - (INTRPT_DISTRIB_LEN - 1) / 2);
 	writeDistribution(g.interruptDistrib, INTRPT_DISTRIB_LEN, scaleZero, 1,
 			&g.lastIntrptFileno, interrupt_distrib_file, last_intrpt_distrib_file);
-
 }
 
 /**
@@ -225,8 +232,8 @@ void buildInterruptDistrib(int intrptDelay){
 
 	idx = intrptDelay - g.scaleCenter;			// Normalize idx to the scale center.
 												// Since that normalizes it to zero,
-	idx = idx + len / 2;						// offset idx to the middle of the
-												// g.interruptDistrib array.
+	idx = idx + len / 3;						// offset idx to the lower third of
+												// the g.interruptDistrib array.
 	if (idx > len){
 		idx = len;
 	}
@@ -270,11 +277,140 @@ int readVerify(void){
 	return val;
 }
 
+int outputSingeEventTime(int tm[]){
+	char timeStr[50];
+
+	int fd = open(last_intrpt_distrib_file, O_RDONLY);
+	if (fd == -1){
+		strcpy(g.strbuf, "File not found: ");
+		strcat(g.strbuf, last_intrpt_distrib_file);
+		strcat(g.strbuf, "\n");
+		printf(g.strbuf);
+		return -1;
+	}
+
+	int sz = FILEBUF_SZ;
+	int len = INTRPT_DISTRIB_LEN;
+	double tmp;
+
+	read(fd, (void *)filebuf, sz);
+	close(fd);
+
+	char *lines[len];
+	double probDistrib[len];
+
+	lines[0] = strtok(filebuf, "\r\n\0");
+	for (int i = 1; i < len; i++){
+		lines[i] = strtok(NULL, "\r\n\0");
+	}
+
+	double sum = 0.0;
+	for (int i = 0; i < len; i++){
+		sscanf(lines[i], "%lf %lf", &tmp, probDistrib + i);
+		sum += probDistrib[i];
+	}
+	double norm = 1.0 / sum;
+	for (int i = 0; i < len; i++){
+		probDistrib[i] *= norm;
+	}
+
+	int lowIdx, maxIdx = 0, hiIdx;
+	double maxVal = 0.0;
+	for (int i = 0; i < len; i++){
+		if (probDistrib[i] > maxVal){
+			maxVal = probDistrib[i];
+			maxIdx = i;
+		}
+	}
+	double tailProb = 0.5 * (1.0 - g.probability);
+
+	double tailSum = 0.0;
+	for (int i = 0; i < len; i++){
+		tailSum += probDistrib[i];
+		if (tailSum > tailProb){
+			lowIdx = i - 1;
+			break;
+		}
+	}
+	tailSum = 0.0;
+	for (int i = len - 1; i >= 0; i--){
+		tailSum += probDistrib[i];
+		if (tailSum > tailProb){
+			hiIdx = i + 1;
+			break;
+		}
+	}
+
+	int lowVal = tm[1] - (hiEdx - maxIdx);
+	int hiVal = tm[1] + (maxIdx - lowIdx);
+
+	double mean = 0.5 * (double)(lowVal + hiVal);
+	int meanVal = (int)round(mean);
+
+	if (meanVal > 1000000){
+		tm[1] = meanVal - 1000000;
+		tm[0] += 1;
+	}
+	else if (meanVal < 0){
+		tm[1] = 1000000 + meanVal;
+		tm[0] -= 1;
+	}
+	else {
+		tm[1] = meanVal;
+	}
+
+	double tol = (double)hiVal - meanVal;
+	int tolerance = (int)round(tol);
+
+	if (g.outFormat == 0){						// Print in date-time format
+		strftime(timeStr, 50, timefmt, localtime((const time_t*)(&tm[0])));
+		printf("%s.%06d +/- 0.%06d\n", timeStr, tm[1], tolerance);
+	}
+	else {										// Print as seconds
+		double time = (double)tm[0] + 1e-6 * tm[1];
+		printf("%lf +/- 0.%06d\n", time, tolerance);
+	}
+
+	return 0;
+}
+
+int outputRepeatingEventTime(int tm[]){
+	char timeStr[50];
+	struct timespec ts2;
+
+	int v = readVerify();
+	if (v == ON_TIME){
+		buildInterruptDistrib(tm[1]);
+	}
+	else if (v == DELAYED){
+		printf("interrupt-timer: Skipping delayed pulse from pulse-generator.\n");
+	}
+	else if (v == SENTINEL){
+		printf("interrupt-timer: Bad read on verify. Read too late.\n");
+	}
+	else if (v == FILE_NOT_FOUND){
+		printf("interrupt-timer Error: Verify file not found.\n");
+		return -1;
+	}
+
+	if (g.outFormat == 0){						// Print in date-time format
+		strftime(timeStr, 50, timefmt, localtime((const time_t*)(&tm[0])));
+		printf("%s.%06d\n", timeStr, tm[1]);
+	}
+	else {										// Print as seconds
+		double time = (double)tm[0] + 1e-6 * tm[1];
+		printf("%lf\n", time);
+	}
+
+	return 0;
+}
+
 int main(int argc, char *argv[]){
 
-	int outFormat = 0;
 	int tm[2];
 	int sysDelay;
+	bool singleEvent = false;
+	bool argRecognized = false;
 
 	if (argc > 1){
 		if (strcmp(argv[1], "load-driver") == 0){
@@ -300,15 +436,40 @@ int main(int argc, char *argv[]){
 				printf("Requires superuser privileges. Please sudo this command.\n");
 				return 0;
 			}
-			printf("interrupt-timer: driver unloaded\n");
+			printf("interrupt-timer: driver unloading\n");
 			driver_unload();
 			return 0;
 		}
-		if (strcmp(argv[1], "-s") == 0){
-			outFormat = 1;
-			goto start;
+
+		for (int i = 1; i < argc; i++){
+			if (strcmp(argv[i], "-s") == 0){
+				g.outFormat = 1;
+				argRecognized = true;
+				continue;
+			}
+			if (strcmp(argv[i], "-p") == 0){
+
+				char *num = strpbrk(argv[i+1], "0123456789.");
+				if (num == NULL){
+					printf("-p must be followed by the probability that the timed\n");
+					printf("event is within the tolerance that will be estimated.\n");
+					return 0;
+				}
+				scanf(argv[2], "%lf", &g.probability);
+				if (g.probability == 0){
+					printf("A positive non-zero probability is required\n");
+					return 0;
+				}
+				if (g.probability > 0.999){
+					g.probability = 0.999;
+				}
+				singleEvent = true;
+				argRecognized = true;
+				continue;
+			}
 		}
-		if (strcmp(argv[1], "-d") == 0){
+
+		if (argRecognized){
 			goto start;
 		}
 
@@ -319,12 +480,18 @@ int main(int argc, char *argv[]){
 		printf("the interrupt will be captured.\n");
 		printf("After loading the driver, calling interrupt-timer\n");
 		printf("causes it to wait for interrupts then output the\n");
-		printf("date-time when one occurs. The following command\n");
-		printf("args modify the format of the date-time output:\n\n");
+		printf("date-time when each occurs. The following command\n");
+		printf("arg modifies the format of the date-time output:\n\n");
 		printf("  -s Outputs seconds since the epoch.\n");
-		printf("  -d Outputs in date format (default).\n");
+		printf("otherwise outputs in date format (default).\n\n");
 		printf("The program will exit on ctrl-c or when no\n");
 		printf("interrupts are received within 5 minutes.\n");
+		printf("Specifying a probability causes interrupt-timer to\n");
+		printf("function as a single event timer that outputs both\n");
+		printf("an event time and an estimated tolerance on that time:\n");
+		printf("  -p <probability>\n");
+		printf("where that is the probability (<= 0.999) that the time is within\n");
+		printf("the estimated tolerance.\n\n");
 
 		return 0;
 	}
@@ -342,19 +509,15 @@ start:
 
 	memset(&g, 0, sizeof(struct interruptTimerGlobalVars));
 
-	struct sched_param param;						// Process must be run as
-	param.sched_priority = 99;						// root to change priority.
-	sched_setscheduler(0, SCHED_FIFO, &param);		// Else, this has no effect.
+	struct sched_param param;								// Process must be run as
+	param.sched_priority = 99;								// root to change priority.
+	sched_setscheduler(0, SCHED_FIFO, &param);				// Else, this has no effect.
 
 	int intrpt_fd = open("/dev/interrupt-timer", O_RDONLY); // Open the interrupt-timer device driver.
 	if (intrpt_fd == -1){
 		printf("interrupt-timer: Driver is not loaded. Exiting.\n");
 		return 1;
 	}
-
-	char timeStr[50];
-	const char *timefmt = "%F %H:%M:%S";
-	struct timespec ts2;
 
 	for (;;){
 		int dvrv = read(intrpt_fd, (void *)tm, 2 * sizeof(int));
@@ -363,40 +526,28 @@ start:
 			if (rv == -1){
 				return 1;
 			}
-			tm[1] -= sysDelay;							// time microseconds corrected for system interrupt sysDelay
-
-			ts2.tv_sec = 0;								// Allow time for pulse-generator to write the verify file.
-			ts2.tv_nsec = 100000;
-			nanosleep(&ts2, NULL);
-
-			int v = readVerify();
-			if (v == ON_TIME){
-				buildInterruptDistrib(tm[1]);
-			}
-			else if (v == DELAYED){
-				printf("interrupt-timer: Skipping delayed pulse from pulse-generator.\n");
-			}
-			else if (v == SENTINEL){
-				printf("interrupt-timer: Bad read on verify. Read too late.\n");
-			}
-			else if (v == FILE_NOT_FOUND){
-				printf("interrupt-timer Error: Verify file not found.\n");
-			}
-
-			if (tm[1] < 0){								// If negative after correction, fix the time.
+			tm[1] -= (sysDelay + HIDDEN_DELAY);				// time microseconds corrected for system interrupt sysDelay
+			if (tm[1] < 0){									// If negative after correction, fix the time.
 				tm[1] = 1000000 + tm[1];
 				tm[0] -= 1;
 			}
 
-			if (outFormat == 0){						// Print in date-time format
+			if (singleEvent == false){
 
-				strftime(timeStr, 50, timefmt, localtime((const time_t*)(&tm[0])));
+				ts2.tv_sec = 0;								// Allow time for pulse-generator to write the verify file.
+				ts2.tv_nsec = 100000;
+				nanosleep(&ts2, NULL);
 
-				printf("%s.%06d\n", timeStr, tm[1]);
+				rv = outputRepeatingEventTime(tm);
+				if (rv == -1){
+					return 1;
+				}
 			}
-			else {										// Print as seconds
-				double time = (double)tm[0] + 1e-6 * tm[1];
-				printf("%lf\n", time);
+			else {
+				rv = outputSingeEventTime(tm);
+				if (rv == -1){
+					return 1;
+				}
 			}
 		}
 		else {
@@ -404,15 +555,17 @@ start:
 			break;
 		}
 
-		g.seconds += 1;
-		if (g.seconds % SECS_PER_MIN == 0){
-			if (g.minutes > 1){
-				writeInterruptDistribFile();
+		if (singleEvent == false){
+			g.seconds += 1;
+			if (g.seconds % SECS_PER_MIN == 0){
+				if (g.minutes > 1){
+					writeInterruptDistribFile();
+				}
+				g.minutes += 1;
 			}
-			g.minutes += 1;
-		}
-		if (g.seconds % SECS_PER_DAY == 0){
-			g.days += 1;
+			if (g.seconds % SECS_PER_DAY == 0){
+				g.days += 1;
+			}
 		}
 	}
 	close(intrpt_fd);
